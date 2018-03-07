8130daed90a610c5ecde41220f6f724b5eef7d00
Karan Bhamra <karan.bhamra@gmail.com>
2018-03-07 14:08:33 -0800

    form now accepts click to select file as well

--- js/dragdrop.js
+++ js/dragdrop.js
@@ -1,158 +1,154 @@
 // create a closure that runs when window is loaded
 (function () {
-	//console.log("Test");
+    // console.log("Test");
 
-	var dropzone = document.getElementById("dropzone");
-	AWS.config.region = 'us-west-2'; // 1. Enter your region
+    var dropzone = document.getElementById('dropzone')
+    AWS.config.region = 'us-west-2' // 1. Enter your region
 
-	AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-		IdentityPoolId: "us-west-2:22fe90da-13ea-44da-b44e-41fd208c0d88" // 2. Enter your identity pool
-	});
+    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+        IdentityPoolId: 'us-west-2:22fe90da-13ea-44da-b44e-41fd208c0d88' // 2. Enter your identity pool
+    })
 
-	AWS.config.credentials.get(function (err) {
-		if (err) alert(err);
-		//console.log(AWS.config.credentials);
-	});
+    AWS.config.credentials.get(function (err) {
+        if (err) alert(err)
+        // console.log(AWS.config.credentials);
+    })
 
-	var bucketName = "comp680testfiles"; // Enter your bucket name
-	var bucket = new AWS.S3({
-		params: {
-			Bucket: bucketName
-		}
-	});
+    var bucketName = 'comp680testfiles' // Enter your bucket name
+    var bucket = new AWS.S3({
+        params: {
+            Bucket: bucketName
+        }
+    })
 
-	var filename;
-	var fileurl;
-
-
-	function prepareUpload(file) {
-		console.log(file);
-
-
-		const MAX_FILE_SIZE = 1.5 * (10 ** 7);
-
-		const VALID_FILE_TYPE = "application/pdf";
-
-		let formData = new FormData();
-
-		formData.append('file', file);
-
-
-		console.log(formData);
-		var uploadFile = formData.get('file');
-		filename = uploadFile.name;
+    var filename
+    var fileurl
 
 
 
-		let dropzone = document.getElementById("dropzone");
 
-		dropzone.innerHTML = '<img src="img/fileicon.png" />' + filename;
+    function prepareUpload (file) {
+        console.log(file)
 
+        const MAX_FILE_SIZE = 1.5 * (10 ** 7)
 
-		let button = document.getElementById("uploadButton");
+        const VALID_FILE_TYPE = 'application/pdf'
 
-		let clearButton = document.getElementById("clearButton");
+        let formData = new FormData()
 
+        formData.append('file', file)
 
-		clearButton.classList.remove("btn-secondary");
-		clearButton.classList.add("btn-danger");
-		clearButton.disabled = false;
+        console.log(formData)
+        var uploadFile = formData.get('file')
+        filename = uploadFile.name
 
+        let dropzone = document.getElementById('dropzone')
 
+        dropzone.innerHTML = '<img src="img/fileicon.png" />' + filename
 
-		button.classList.remove("btn-secondary");
-		button.classList.add("btn-success");
-		button.disabled = false;
+        let button = document.getElementById('uploadButton')
 
-		// console.log(uploadFile.name);
-		// console.log(uploadFile.size);
+        let clearButton = document.getElementById('clearButton')
 
-		// if (uploadFile.size < MAX_FILE_SIZE) {
-		// 	console.log("Valid filesize");
-		// } else {
-		// 	console.log("Invalid filesize");
-		// }
+        clearButton.classList.remove('btn-secondary')
+        clearButton.classList.add('btn-danger')
+        clearButton.disabled = false
 
-		// if (uploadFile.type === VALID_FILE_TYPE) {
-		// 	console.log("Uploading PDF");
-		// } else {
-		// 	console.log("File MUST be a PDF.");
-		// }
-		//fileToUpload = uploadFile;
+        button.classList.remove('btn-secondary')
+        button.classList.add('btn-success')
+        button.disabled = false
 
-		uploadToS3(uploadFile);
+        // console.log(uploadFile.name);
+        // console.log(uploadFile.size);
 
-	}
+        // if (uploadFile.size < MAX_FILE_SIZE) {
+        // 	console.log("Valid filesize");
+        // } else {
+        // 	console.log("Invalid filesize");
+        // }
 
-	function uploadToS3(file) {
+        // if (uploadFile.type === VALID_FILE_TYPE) {
+        // 	console.log("Uploading PDF");
+        // } else {
+        // 	console.log("File MUST be a PDF.");
+        // }
+        // fileToUpload = uploadFile;
 
+        uploadToS3(uploadFile)
+    }
 
-		let button = document.getElementById("uploadButton");
+    function uploadToS3 (file) {
+        let button = document.getElementById('uploadButton')
 
-		button.addEventListener('click', function (params) {
+        button.addEventListener('click', function (params) {
+            //	results.innerHTML = '';
+            var objKey = 'testing/' + file.name
+            var params = {
+                Key: objKey,
+                ContentType: file.type,
+                Body: file,
+                ACL: 'public-read'
+            }
 
-			//	results.innerHTML = '';
-			var objKey = 'testing/' + file.name;
-			var params = {
-				Key: objKey,
-				ContentType: file.type,
-				Body: file,
-				ACL: 'public-read'
-			};
+            bucket.putObject(params, function (err, data) {
+                if (err) {
+                    console.log('ERROR: ' + err)
+                } else {
+                    console.log('Upload success.')
+                    fileurl = 'https://s3-us-west-2.amazonaws.com/comp680testfiles/testing/' + filename
+                    alert('File ' + filename + ' uploaded.\n' + 'File can be accessed from: ' + fileurl)
+                    location.reload()
+                }
+                // if (err) {
+                // 	results.innerHTML = 'ERROR: ' + err;
+                // } else {
+                // 	listObjs();
+                // }
+            })
+            // fileurl = "https://s3-us-west-2.amazonaws.com/comp680testfiles/testing/" + filename;
+            // alert("File " + filename + " uploaded.\n" + "File can be accessed from: " + fileurl);
+            // location.reload();
+        })
+    }
 
-			bucket.putObject(params, function (err, data) {
-
-				if (err) {
-					console.log("ERROR: " + err);
-				} else {
-					console.log("Upload success.");
-                    fileurl = "https://s3-us-west-2.amazonaws.com/comp680testfiles/testing/" + filename;
-                    alert("File " + filename + " uploaded.\n" + "File can be accessed from: " + fileurl);
-					location.reload();
-				}
-				// if (err) {
-				// 	results.innerHTML = 'ERROR: ' + err;
-				// } else {
-				// 	listObjs();
-				// }
-			});
-			//fileurl = "https://s3-us-west-2.amazonaws.com/comp680testfiles/testing/" + filename;
-			//alert("File " + filename + " uploaded.\n" + "File can be accessed from: " + fileurl);
-			//location.reload();
-		});
-	}
-
-
-
-	// on drop send a list of files to upload func
-	dropzone.ondrop = function (e) {
-		e.preventDefault();	// make sure any dropped object isnt loaded and displayed
-		this.className = "dropzone";
-		//console.log(e.dataTransfer.files);
-
-		if (e.dataTransfer.files.length > 1) {
-			alert("Only one file allowed.");
-		}
-		else {
-
-			prepareUpload(e.dataTransfer.files[0]);
-		}
+    let filebrowser = document.getElementById("fileBrowser")
 
 
-	};
+    filebrowser.addEventListener('change', function () {
 
-	// color it black
-	dropzone.ondragover = function () {
+        let fileList = this.files
+        let file = fileList[0]
+        //console.log(fileList[0]);
+        prepareUpload(file)
 
-		this.className = "dropzone dragover";
 
-		return false;
-	}
+    });
 
-	// color it back to gray
-	dropzone.ondragleave = function () {
-		this.className = "dropzone";
+    // on drop send a list of files to upload func
+    dropzone.ondrop = function (e) {
+        e.preventDefault() // make sure any dropped object isnt loaded and displayed
+        this.className = 'dropzone'
+        // console.log(e.dataTransfer.files);
 
-		return false;
-	}
-}());
+        if (e.dataTransfer.files.length > 1) {
+            alert('Only one file allowed.')
+        } else {
+            //console.log(e.dataTransfer.files[0]);
+            prepareUpload(e.dataTransfer.files[0])
+        }
+    }
+
+    // color it black
+    dropzone.ondragover = function () {
+        this.className = 'dropzone dragover'
+
+        return false
+    }
+
+    // color it back to gray
+    dropzone.ondragleave = function () {
+        this.className = 'dropzone'
+
+        return false
+    }
+}())
